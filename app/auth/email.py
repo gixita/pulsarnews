772f90e995faa66cbdb9081454287ed403868577
gixita <kris.laermans@gmail.com>
2021-02-15 21:06:11 +0100

    enable mail for email verification

--- app/auth/email.py
+++ app/auth/email.py
@@ -18,16 +18,16 @@ def send_password_reset_email(user):
 
 def send_verification_email(user):
     token = user.get_mail_verification_token()
-    link = 'http://localhost:5000/auth/verify_account?token='+ token 
-    print(link)
-    # send_email(
-    #     subject="Flasknews password recovery",
-    #     sender=current_app.config["MAIL_ADMIN_ADDRESS"],
-    #     recipients=[user.email],
-    #     text_body=render_template(
-    #         "email/reset_password.txt", user=user, token=token
-    #     ),
-    #     html_body=render_template(
-    #         "email/reset_password.html", user=user, token=token
-    #     ),
-    # )
+    # link = 'http://localhost:5000/auth/verify_account?token='+ token 
+    # print(link)
+    send_email(
+        subject="Flasknews password recovery",
+        sender=current_app.config["MAIL_ADMIN_ADDRESS"],
+        recipients=[user.email],
+        text_body=render_template(
+            "email/email_verification.txt", user=user, token=token
+        ),
+        html_body=render_template(
+            "email/email_verification.html", user=user, token=token
+        ),
+    )
