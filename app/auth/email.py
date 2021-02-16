f6aa6baa1b61a2ed866ca515fd66ec340ac2ff7f
gixita <kris.laermans@gmail.com>
2021-02-16 07:58:01 +0100

    improve email verification email layout

--- app/auth/email.py
+++ app/auth/email.py
@@ -1,5 +1,4 @@
 from flask import render_template, current_app
-# from app.email import send_email
 import boto3
 
 def send_email(app, recipients, sender=None, subject='', text='', html=''):
@@ -26,42 +25,24 @@ def send_email(app, recipients, sender=None, subject='', text='', html=''):
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
-    send_email(
-        subject="PulsarNews password recovery",
-        sender=current_app.config["SES_EMAIL_SOURCE"],
-        recipients=[user.email],
-        text_body=render_template(
-            "email/reset_password.txt", user=user, token=token
-        ),
-        html_body=render_template(
-            "email/reset_password.html", user=user, token=token
-        ),
-    )
-
-def send_verification_email(user):
-    token = user.get_mail_verification_token()
-    # link = 'http://localhost:5000/auth/verify_account?token='+ token 
-    # print(link)
     recipients=[user.email]
-    subject="PulsarNews email verification"
-
-    # You can render the message using Jinja2
-    html = render_template('email/email_verification.html', user=user, token=token)
-
+    subject="Password reset - PulsarNews"
+    html = render_template('email/reset_password.html', user=user, token=token)
     send_email(current_app,
         recipients=recipients,
         subject=subject,
         html=html
         )
-    
-    # send_email(
-    #     subject="PulsarNews email verification",
-    #     sender=current_app.config["MAIL_ADMIN_ADDRESS"],
-    #     recipients=[user.email],
-    #     text_body=render_template(
-    #         "email/email_verification.txt", user=user, token=token
-    #     ),
-    #     html_body=render_template(
-    #         "email/email_verification.html", user=user, token=token
-    #     ),
-    # )
+
+def send_verification_email(user):
+    token = user.get_mail_verification_token()
+    # # link = 'http://localhost:5000/auth/verify_account?token='+ token 
+    # print(link)
+    recipients=[user.email]
+    subject="Email verification - PulsarNews"
+    html = render_template('email/email_verification.html', user=user, token=token)
+    send_email(current_app,
+        recipients=recipients,
+        subject=subject,
+        html=html
+        )
