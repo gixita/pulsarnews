0ac83904607514a77ad8355f2e548c02d0bf80fd
gixita <kris.laermans@gmail.com>
2021-03-01 15:25:42 +0100

    increase session cookies security

--- config.py
+++ config.py
@@ -24,6 +24,10 @@ class Config(object):
     LOG_TO_STDOUT = os.environ.get("LOG_TO_STDOUT")
     WTF_CSRF_ENABLED = True
     SERVER_NAME = os.environ.get("SERVER_NAME")
+    SESSION_COOKIE_SECURE = True
+    REMEMBER_COOKIE_SECURE = True
+    SESSION_COOKIE_HTTPONLY = True
+    REMEMBER_COOKIE_HTTPONLY = True
     
     #Azure auth
     SESSION_TYPE = "filesystem" 
